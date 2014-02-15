d7dffe09c8685cb61dbd34ca070a2d0d0ae4f8f4
Max <mczonk@gmail.com>
2014-02-15 11:10:04 +0100

    Generalized version for linked programs

--- GLPProgram+LinkedProgram.h
+++ GLPProgram+LinkedProgram.h
@@ -1,5 +1,6 @@
 #import "GLPProgram.h"
 
+@class GLPShader;
 
 @interface GLPProgram (LinkedProgram)
 
@@ -8,5 +9,7 @@
 + (instancetype)linkedProgramWithVertexShaderFile:(NSString *)vertexShaderFile fragmentShaderFile:(NSString *)fragmentShaderFile attributes:(NSDictionary *)attributes error:(NSError **)error;
 
 + (instancetype)linkedProgramWithVertexShaderResource:(NSString *)vertexShaderResource fragementShaderResource:(NSString *)fragmentShaderResource bundle:(NSBundle *)bundle attributes:(NSDictionary *)attributes error:(NSError **)error;
+
++ (instancetype)linkedProgramWithVertexShader:(GLPShader *)vertexShader fragmentShader:(GLPShader *)fragmentShader attributes:(NSDictionary *)attributes error:(NSError **)error;
 
 @end
--- GLPProgram+LinkedProgram.m
+++ GLPProgram+LinkedProgram.m
@@ -7,120 +7,65 @@
 
 + (instancetype)linkedProgramWithVertexShaderSource:(NSString *)vertexShaderSource fragmentShaderSource:(NSString *)fragmentShaderSource attributes:(NSDictionary *)attributes error:(NSError **)error
 {
-	GLPProgram *program = [self program];
-	
-	GLPShader *vertexShader = nil;
-	GLPShader *fragmentShader = nil;
-	
-	if(vertexShaderSource != nil)
-	{
-		vertexShader = [GLPShader compiledVertexShaderWithSource:vertexShaderSource error:error];
-		if(vertexShader == nil)
-		{
-			return nil;
-		}
-		[program attachShader:vertexShader];
-	}
-	
-	if(fragmentShaderSource != nil)
-	{
-		fragmentShader = [GLPShader compiledFragmentShaderWithSource:fragmentShaderSource error:error];
-		if(fragmentShader == nil)
-		{
-			return nil;
-		}
-		[program attachShader:fragmentShader];
-	}
-	
-	[program bindAttributes:attributes];
-	
-	if(![program link:error])
+	GLPShader *vertexShader = [GLPShader compiledVertexShaderWithSource:vertexShaderSource error:error];
+	if(vertexShader == nil)
 	{
 		return nil;
 	}
 	
-	if(vertexShader != nil)
+	GLPShader *fragmentShader = [GLPShader compiledFragmentShaderWithSource:fragmentShaderSource error:error];
+	if(fragmentShader == nil)
 	{
-		[program detachShader:vertexShader];
-	}
-	if(fragmentShader != nil)
-	{
-		[program detachShader:fragmentShader];
+		return nil;
 	}
 	
-	return program;
+	return [self linkedProgramWithVertexShader:vertexShader fragmentShader:fragmentShader attributes:attributes error:error];
 }
 
 + (instancetype)linkedProgramWithVertexShaderFile:(NSString *)vertexShaderFile fragmentShaderFile:(NSString *)fragmentShaderFile attributes:(NSDictionary *)attributes error:(NSError **)error
 {
-	GLPProgram *program = [self program];
-	
-	GLPShader* vertexShader = nil;
-	GLPShader* fragmentShader = nil;
-	
-	if(vertexShaderFile != nil)
-	{
-		vertexShader = [GLPShader compiledVertexShaderWithContentsOfFile:vertexShaderFile error:error];
-		if(vertexShader == nil)
-		{
-			return nil;
-		}
-		[program attachShader:vertexShader];
-	}
-	
-	if(fragmentShaderFile != nil)
-	{
-		fragmentShader = [GLPShader compiledFragmentShaderWithContentsOfFile:fragmentShaderFile error:error];
-		if(fragmentShader == nil)
-		{
-			return nil;
-		}
-		[program attachShader:fragmentShader];
-	}
-	
-	[program bindAttributes:attributes];
-	
-	if(![program link:error])
+	GLPShader* vertexShader = [GLPShader compiledVertexShaderWithContentsOfFile:vertexShaderFile error:error];
+	if(vertexShader == nil)
 	{
 		return nil;
 	}
 	
-	if(vertexShader != nil)
+	GLPShader* fragmentShader = [GLPShader compiledFragmentShaderWithContentsOfFile:fragmentShaderFile error:error];
+	if(fragmentShader == nil)
 	{
-		[program detachShader:vertexShader];
+		return nil;
 	}
-	if(fragmentShader != nil)
-	{
-		[program detachShader:fragmentShader];
-	}
-	
-	return program;
+
+	return [self linkedProgramWithVertexShader:vertexShader fragmentShader:fragmentShader attributes:attributes error:error];
 }
 
 + (instancetype)linkedProgramWithVertexShaderResource:(NSString *)vertexShaderResource fragementShaderResource:(NSString *)fragmentShaderResource bundle:(NSBundle *)bundle attributes:(NSDictionary *)attributes error:(NSError **)error
 {
-	GLPProgram *program = [self program];
-
-	GLPShader* vertexShader = nil;
-	GLPShader* fragmentShader = nil;
-	
-	if(vertexShaderResource != nil)
+	GLPShader *vertexShader = [GLPShader compiledVertexShaderWithResource:vertexShaderResource bundle:bundle error:error];
+	if(vertexShader == nil)
 	{
-		vertexShader = [GLPShader compiledVertexShaderWithResource:vertexShaderResource bundle:bundle error:error];
-		if(vertexShader == nil)
-		{
-			return nil;
-		}
-		[program attachShader:vertexShader];
+		return nil;
 	}
 	
-	if(fragmentShaderResource != nil)
+	GLPShader *fragmentShader = [GLPShader compiledFragmentShaderWithResource:fragmentShaderResource bundle:bundle error:error];
+	if(fragmentShader == nil)
 	{
-		fragmentShader = [GLPShader compiledFragmentShaderWithResource:fragmentShaderResource bundle:bundle error:error];
-		if(fragmentShader == nil)
-		{
-			return nil;
-		}
+		return nil;
+	}
+	
+	return [self linkedProgramWithVertexShader:vertexShader fragmentShader:fragmentShader attributes:attributes error:error];
+}
+
++ (instancetype)linkedProgramWithVertexShader:(GLPShader *)vertexShader fragmentShader:(GLPShader *)fragmentShader attributes:(NSDictionary *)attributes error:(NSError **)error
+{
+	GLPProgram *program = [self program];
+	
+	if(vertexShader != nil)
+	{
+		[program attachShader:vertexShader];
+	}
+	if(fragmentShader != nil)
+	{
 		[program attachShader:fragmentShader];
 	}
 	
