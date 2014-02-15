afe3ffbb6684b805811988766f6b6152447cd6c8
Max <mczonk@gmail.com>
2014-02-15 11:55:00 +0100

    Added new files to pch

--- OpenGLPlus.h
+++ OpenGLPlus.h
@@ -22,25 +22,41 @@
 
 
 #import <OpenGLPlus/GLPBuffer.h>
+#import <OpenGLPlus/GLPBuffer+ObjectLabel.h>
+
 #import <OpenGLPlus/GLPFramebuffer.h>
+#import <OpenGLPlus/GLPFramebuffer+ObjectLabel.h>
+
 #import <OpenGLPlus/GLPObjectLabel.h>
+
 #import <OpenGLPlus/GLPProgram.h>
 #import <OpenGLPlus/GLPProgram+DebugInformation.h>
 #import <OpenGLPlus/GLPProgram+LinkedProgram.h>
 #import <OpenGLPlus/GLPProgram+ObjectLabel.h>
 #import <OpenGLPlus/GLPProgram+SetUniform.h>
+#import <OpenGLPlus/GLPProgram+TransformFeedback.h>
 #import <OpenGLPlus/GLPProgram+XML.h>
+
 #import <OpenGLPlus/GLPRenderbuffer.h>
+#import <OpenGLPlus/GLPRenderbuffer+ObjectLabel.h>
+
 #import <OpenGLPlus/GLPRendertarget.h>
+
 #import <OpenGLPlus/GLPShader.h>
 #import <OpenGLPlus/GLPShader+CompiledShader.h>
+#import <OpenGLPlus/GLPShader+ObjectLabel.h>
+
 #import <OpenGLPlus/GLPSync.h>
 #import <OpenGLPlus/GLPSync+ObjectLabel.h>
+
 #import <OpenGLPlus/GLPTexture.h>
 #import <OpenGLPlus/GLPTexture+CGImage.h>
 #import <OpenGLPlus/GLPTexture+NamedTexture.h>
+#import <OpenGLPlus/GLPTexture+ObjectLabel.h>
 #import <OpenGLPlus/GLPTexture+UIImage.h>
+
 #import <OpenGLPlus/GLPVertexArray.h>
+#import <OpenGLPlus/GLPVertexArray+ObjectLabel.h>
 
 #import <OpenGLPlus/NSError+GLPError.h>
 #import <OpenGLPlus/NSError+GLPProgramLinkerError.h>
@@ -49,4 +65,5 @@
 #import <OpenGLPlus/GLKTextureInfo+GLPTexture.h>
 
 #import <OpenGLPlus/EAGLContext+GLPBestRenderingAPI.h>
+
 #import <OpenGLPlus/EAGLPContextStack.h>
